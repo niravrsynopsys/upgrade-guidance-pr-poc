8394d0e5e5a8cc0c7a7f4135e3131c9caf03f9ff
Nirav Raje <niravr@synopsys.com>
2023-03-02 23:35:09 -0500

    Generate intermediate vulnerability component dataset

---
+++ src/main/java/org/poc/Main.java
@@ -0,0 +1,41 @@
+package org.poc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.File;
+import java.io.PrintWriter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            String inputFilePath = "/jsonPayloadDetect.json";
+            InputStream inputStream = Main.class.getResourceAsStream(inputFilePath);
+
+            VulnComponentDataset vulnComponentDataset = new VulnComponentDataset();
+            String jsonData = new String(inputStream.readAllBytes());
+            JSONObject jsonObject = new JSONObject(jsonData);
+
+            // Part 1: Generate vulnerability-component dataset
+            JSONObject intermediateResult = vulnComponentDataset.generateVulnComponentDataset(jsonObject);
+
+             System.out.println("\nResult:\n" + intermediateResult.toString(4));
+
+            // Write the intermediate output to a folder
+            File targetDir = new File("target/output-files");
+            targetDir.mkdirs();
+            File outputFile = new File(targetDir, "intermediate-output.json");
+            PrintWriter fileWriter = new PrintWriter(outputFile);
+            fileWriter.println(intermediateResult.toString(4));
+            fileWriter.close();
+
+            // Part 2: Find and update the locations of each component
+
+        } catch (IOException | JSONException e) {
+            System.out.println("An error occurred while reading the file.");
+            e.printStackTrace();
+        }
+    }
+}
